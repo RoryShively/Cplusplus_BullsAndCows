e5eafa00a567a2371b1fe8d6981bd5459ea728bd
Rory Shively <roryshively1@gmail.com>
2016-04-24 17:18:54 -0500

    added loop for guessing word

--- BullCow/main.cpp
+++ BullCow/main.cpp
@@ -9,15 +9,21 @@ string GetGuessAndPrintBack();
 int main(void)
 {
     PrintIntro();
-    GetGuessAndPrintBack();
-    GetGuessAndPrintBack();
     
-    cout << endl;
+    // Loop for the number of turns asking for guesses
+    constexpr int NUMBER_OF_TERMS = 5;
+    for (int i=0; i<NUMBER_OF_TERMS; i++)
+    {
+        GetGuessAndPrintBack();
+        cout << endl;
+    }
+    
     return 0;
 }
 
 // Introduce the game
-void PrintIntro() {
+void PrintIntro()
+{
     constexpr int WORD_LENGTH = 5;
     cout << "Welcome to Bulls and Cows, a fantasic word game.\n";
     cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of??\n";
@@ -26,7 +32,8 @@ void PrintIntro() {
 }
 
 // Get the guess from the player
-string GetGuessAndPrintBack() {
+string GetGuessAndPrintBack()
+{
     string Guess = "";
     cout << "What's your guess? ";
     getline(cin, Guess);
