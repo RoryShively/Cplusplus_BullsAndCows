439378214258883f1c217a076fe4d66944544752
Rory Shively <roryshively1@gmail.com>
2016-04-24 14:24:28 -0500

    Initial Commit

---
+++ BullCow/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCow
+//
+//  Created by Rory Shively on 4/24/16.
+//  Copyright © 2016 Rory Shively. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
